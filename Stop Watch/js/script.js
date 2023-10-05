660a8bcaaa3b5e2be266ef98358fc056f28cab37
paranietharan74 <paranietharan5024@gmail.com>
2023-10-05 19:50:24 +0530

    JS logic code finished

--- Stop Watch/js/script.js
+++ Stop Watch/js/script.js
@@ -2,23 +2,52 @@ const btnStart = document.querySelector('.start');
 const btnStop = document.querySelector('.stop');
 const btnReset = document.querySelector('.reset');
 
-let hrs=min=sec=miliSec=0;
+let hrs = min = sec = miliSec = 0;
+
+let StartTimer;
 
 function UpdateDisplay() {
-    // format time
-    hrs = hrs<10?'0'+hrs:hrs;
-    min = min<10?'0'+min:min;
-    sec = sec<10?'0'+sec:sec;
-    miliSec = miliSec<10?'0'+miliSec:miliSec;
+    // Format time
+    const Thrs = hrs < 10 ? '0' + hrs : hrs;
+    const Tmin = min < 10 ? '0' + min : min;
+    const Tsec = sec < 10 ? '0' + sec : sec;
+    const TmiliSec = miliSec < 10 ? '0' + miliSec : miliSec;
 
-    document.querySelector('.hrs').innerText=hrs;
-    document.querySelector('.min').innerText=min;
-    document.querySelector('.sec').innerText=sec;
-    document.querySelector('.miliSec').innerText=miliSec;
+    document.querySelector('.hrs').innerText = Thrs;
+    document.querySelector('.min').innerText = Tmin;
+    document.querySelector('.sec').innerText = Tsec;
+    document.querySelector('.miliSec').innerText = TmiliSec;
 }
 
-btnStart.addEventListener('click',UpdateDisplay)
-btnStop.addEventListener('click', ()=>{alert('Stop')})
-btnReset.addEventListener('click', ()=>{
-    hrs=min=sec=miliSec=0;
-})
+btnStart.addEventListener('click', () => {
+    StartTimer = setInterval(() => {
+        miliSec++;
+        if (miliSec === 100) {
+            sec++;
+            miliSec = 0;
+        }
+        if (sec === 60) {
+            min++;
+            sec = 0;
+        }
+        if (min === 60) {
+            hrs++;
+            min = 0;
+        }
+        UpdateDisplay();
+    }, 10);
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(StartTimer);
+});
+
+btnReset.addEventListener('click', () => {
+    try {
+        clearInterval(StartTimer);
+    } catch (error) {
+        console.error('An error occurred while stopping the timer:', error);
+    }
+    hrs = min = sec = miliSec = 0;
+    UpdateDisplay();
+});
