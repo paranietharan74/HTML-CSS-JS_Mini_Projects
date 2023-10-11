629c812689d55391c64063811bdaed34bce6cf11
Paranietharan Palasuntharam <paranietharan5024@gmail.com>
2023-10-11 20:12:11 +0530

    js work finished

--- CSS Generator/js/script.js
+++ CSS Generator/js/script.js
@@ -1,1 +1,35 @@
-document.addEventListener("DOMContentLoaded");
+document.addEventListener("DOMContentLoaded", () => {
+    const allBorder = document.getElementById('all');
+    const all_val = document.getElementById('all-val');
+    const code = document.getElementById('code');
+    const container = document.querySelector('.container');
+    const btnCopy = document.querySelector('#btnCopy');
+
+    var all_radius = 10;
+    var coding;
+
+    function allBordersUpdate() {
+        all_radius = allBorder.value;
+        all_val.innerText = all_radius+"px";
+
+        coding = `border-radius: ${all_radius}px;`;
+
+        code.value=coding;
+        container.style.cssText=coding;
+    }
+
+    function updateCode(){
+        //
+    }
+
+    allBorder.addEventListener("mousemove", allBordersUpdate);
+    allBorder.addEventListener("change", allBordersUpdate);
+
+    btnCopy.addEventListener('click', ()=>{
+        document.querySelector('textarea').select();
+        document.execCommand('copy');
+        alert('text copied');
+    });
+
+    allBordersUpdate();
+});
